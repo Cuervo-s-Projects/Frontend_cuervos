4f61a13ab5f8ed8b0d8c8343386d9f1219d30919
David Giraldo <jesdavgirgom97@gmail.com>
2025-06-17 21:56:31 -0500

    Update LoginForm and SIgnupForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,20 +10,28 @@ export default function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     setMessage('');
+    console.log('Formulario enviado');
 
     try{
-      const res = await axios.post(`${import.meta.env.VITE_API_URL}/login`, {
+      const res = await axios.post(`http://127.0.0.1:5000/api/login`, {
         email,
         password,
       });
-      localStorage.setItem('token', res.data.access_token);
-      console.log(res.data.access_token)
-      setMessage('✅ Login exitoso');
-    } catch (err) {
+
+      if (res.status === 200){
+        localStorage.setItem('token', res.data.access_token);
+        console.log(res.data.access_token)
+        setEmail('')
+        setPassword('')
+        setMessage('✅ Login exitoso');
+      }else{
+        console.log("Credenciales inválidas")
+        setMessage("❌ Credenciales inválidas")
+      }
+    }catch (err) {
       setMessage('❌ Credenciales inválidas');
     }   
   }
@@ -36,6 +44,7 @@ export default function LoginForm() {
         placeholder="Correo"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        required
       />
       <input
         className="login-input"
@@ -43,6 +52,7 @@ export default function LoginForm() {
         placeholder="Contraseña"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        required
       />
       <button className="login-button" type="submit">Iniciar Sesión</button>
       {message && <p className="login-message">{message}</p>}
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -4,22 +4,26 @@ import axios from 'axios';
 
 export default function SignupForm() {
     const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [password_confirm, setPassword_confirm] = useState('');
     const [message, setMessage] = useState('');
-
     const handleSubmit = async (e) => {
         e.preventDefault();
-        setMessage('');
-
+        console.log('Formulario enviado');
         try {
-            const res = await axios.post(`${import.meta.env.VITE_API_URL}/signup`, {
+            const res = await axios.post(`http://127.0.0.1:5000/api/signup`, {
                 email,
+                username,
                 password,
+                password_confirm
             });
-
+            console.log(res.request)
             setMessage('✅ Registro exitoso. Ya puedes iniciar sesión.');
             setEmail('');
             setPassword('');
+            setPassword_confirm('');
+            setUsername('');
         } catch (err) {
             setMessage('❌ Error al registrar. Verifica los datos o intenta más tarde.');
         }
@@ -37,6 +41,16 @@ export default function SignupForm() {
                 required
             /><br />
 
+            <label htmlFor="username">Nombre de usuario:</label><br />
+            <input
+                type="username"
+                id="username"
+                placeholder="user"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            /><br />
+
             <label htmlFor="password">Contraseña:</label><br />
             <input
                 type="password"
@@ -44,6 +58,16 @@ export default function SignupForm() {
                 placeholder="Contraseña segura"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                required
+            /><br />
+
+            <label htmlFor="password_confirm">Contraseña:</label><br />
+            <input
+                type="password"
+                id="password_confirm"
+                placeholder="Contraseña segura"
+                value={password_confirm}
+                onChange={(e) => setPassword_confirm(e.target.value)}
                 required
             /><br />
 
